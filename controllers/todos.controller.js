bff80f8957fb27dab73730baa70bf3579e46f672
res Arfita <hresaarfita@gmail.com>
2024-10-17 06:50:49 +0700

    add routes for controller

---
+++ controllers/todos.controller.js
@@ -0,0 +1,7 @@
+module.exports = {
+    getAllTodo: (req, res) => {}
+    getTodoById: (req, res) => {}
+    addTodo: (req, res) => {}
+    editTodoById: (req, res) => {}
+    deleteTodoById: (req, res) => {}
+}
