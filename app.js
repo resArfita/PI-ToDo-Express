7254128c624ae580d427039540db04a7ba98a4d2
res Arfita <hresaarfita@gmail.com>
2024-10-17 16:01:19 +0700

    buat connection ke db mongoose

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 3000
+// const dotenv = require("dotenv")
+// const dbConnection = require("./db/conect")
+
+// app.get("/", (req, res) => {
+//     res.json({
+//         message: "Welcome"
+//     })
+// })
+
+// dotenv.config()
+
 
 const allRoute = require("./routes")
+const db = require("./db")
+
+db.then(() => {
+    console.log("Berhasil Connect ke db")
+}).catch((e) => {
+    console.log("Gagal Connect db")
+    console.log(e)
+})
 
 app.use(express.json())
 app.use(allRoute)
 
 app.listen(PORT, () => {
-    console.log("Server Running in port" + PORT)
+    console.log("Server Running in port " + PORT)
 })
+
+// dbConnection()
